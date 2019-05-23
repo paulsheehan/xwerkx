2f4a7afce1bb5436e4808a1b8be7f3923a8990cc
paulsheehan <p.sheehan94@gmail.com>
2019-05-23 21:45:09 +0100

    Fixed SVG display and list item border

--- src/components/AlertListItem.js
+++ src/components/AlertListItem.js
@@ -7,7 +7,10 @@ import ReactSVG from 'react-svg'
 const AlertsList = (props) => {
     return (
         <li className="alerts-list__item">
-            <img className="alerts-list__icon" src={cake} alt="Birthday Cake" />
+            <div className="alerts-list__icon-container">
+                <img className="alerts-list__icon" src={cake} alt="Birthday Cake" />
+            </div>
+            
             <div>{props.alert.name}</div>
             <div className="alerts-list__message">{props.alert.message}</div>
             <div>{props.alert.date}</div>
